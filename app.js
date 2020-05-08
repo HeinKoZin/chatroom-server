786ba875ab7586b1f62c076e0aa93fd1ff34d871
HeinKoZin <heinkozin4@gmail.com>
2020-05-08 16:26:43 +0630

    First

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var socket = require('socket.io');
+var PORT = 5500;
+var app = express();
+var server = app.listen(PORT, () => {
+    console.log('listening')
+});
+
+
+app.use(express.static('public'));
+var io = socket(server);
+
+
+let messages = []
+let initialLength = messages.length;
+
+io.on('connection', (socket) => {
+
+    socket.broadcast.emit('messages', { message: `${socket.id} is conneted.`, by: 'other' });
+    socket.emit('messages', { message: 'you are connected', by: 'me' });
+    socket.on('send', (e) => {
+        // messages.push(e);
+        socket.emit('messages', { message: e, by: 'me' });
+        socket.broadcast.emit('messages', { message: e, by: 'other' });
+    });
+
+});
